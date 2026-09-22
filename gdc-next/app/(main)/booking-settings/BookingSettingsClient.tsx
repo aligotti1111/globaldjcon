d12c657ea0f26e9f1337f11fca648712c188ec72
aligotti1111 <ligotti111@gmail.com>
2026-09-22 04:41:12 -0400

    Update BookingSettingsClient.tsx

--- gdc-next/app/(main)/booking-settings/BookingSettingsClient.tsx
+++ gdc-next/app/(main)/booking-settings/BookingSettingsClient.tsx
@@ -359,12 +359,6 @@ export default function BookingSettingsClient({ initialProfile, hasBookingAccess
         }
       `}</style>
       <div className={styles.headerRow}>
-        <Link href="/" className={styles.backLink}>
-          <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-            <path d="M19 12H5M12 19l-7-7 7-7" />
-          </svg>
-          Back to Directory
-        </Link>
         {autosaveStatus !== 'idle' && (
           <span
             style={{
